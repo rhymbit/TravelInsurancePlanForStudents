0aad78f9368361cdceba9b244ba308120925575f
prateek332 <prateekn332@outlook.com>
2021-06-18 23:16:36 +0530

    Travel Insurance Test (Still incomplete) Automation completed till the part where we have to collect top 3 low priced insurance plans. So still have to work on collecting top 3 lowest price plans. 😁

--- src/main/java/com/cognizant/travelinsurance/TravelInsurancePO.java
+++ src/main/java/com/cognizant/travelinsurance/TravelInsurancePO.java
@@ -1,13 +1,72 @@
 package com.cognizant.travelinsurance;
 
 
+import com.cognizant.Utilities.Navigate;
 import com.cognizant.homepage.HomePagePO;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class TravelInsurancePO {
     public WebDriver driver = null;
 
+    public By searchCountryBy = By.xpath("//input[@id='myText']");
+    public By countryListingBy = By.xpath("//div[@class='country_listing']");
+    public By ageSelector0By = By.xpath("//select[@id='Traveller_0']");
+    public By ageSelector1By = By.xpath("//select[@id='Traveller_1']");
+    public By addAnotherTravelerBy = By.xpath("//button[@class='secandry_button add_travller']");
+    public By nextBy = By.xpath("//button[@class='next next_btn_step']");
+    public By dateFirstBy = By.xpath("//td[@class='CalendarDay CalendarDay_1 CalendarDay__default"
+            +" CalendarDay__default_2 CalendarDay__today CalendarDay__today_3']");
+    public By dateSecondBy = By.xpath("//div[contains(@class,'row pre_main booking-section')]//"+
+            "div[contains(@class,'row')]//div[contains(@class,'')]//div[3]//div[1]//table[1]//tbody[1]//tr[4]//td[3]");
+    public By proceedBy = By.xpath("//button[@id='GetQuoteButton']");
+    public By mobileNumberBy = By.xpath("//input[@id='txtMobileNo']");
+    public By viewPlansBy = By.xpath("//button[@id='GetQuoteButton']");
+
     public TravelInsurancePO(WebDriver driver){
         this.driver = driver;
+    }
+
+    public void searchCountry(String country) {
+        Navigate.waitForElement(driver,searchCountryBy);
+        Navigate.sendKeys(driver.findElement(searchCountryBy), country);
+        Navigate.waitForElement(driver, countryListingBy);
+        Navigate.clickElement(driver.findElement(countryListingBy));
+    }
+
+    public void selectAge(String age, By locator) {
+        Navigate.waitForElement(driver, locator);
+        Select select = new Select(driver.findElement(locator));
+        select.selectByValue(age);
+    }
+
+    public void addAnotherTraveler() {
+        Navigate.waitForElement(driver, addAnotherTravelerBy);
+        Navigate.clickElement(driver.findElement(addAnotherTravelerBy));
+    }
+
+    public void clickNext() {
+        Navigate.clickElement(driver.findElement(nextBy));
+    }
+
+    public void selectCalendarDates() {
+        Navigate.waitForElement(driver, dateFirstBy);
+        Navigate.clickElement(driver.findElement(dateFirstBy));
+        Navigate.clickElement(driver.findElement(dateSecondBy));
+    }
+
+    public void clickProceedButton() {
+        Navigate.clickElement(driver.findElement(proceedBy));
+    }
+
+    public void enterMobileNumber(String moblieNumber) {
+        Navigate.waitForElement(driver, mobileNumberBy);
+        Navigate.sendKeys(driver.findElement(mobileNumberBy), moblieNumber);
+    }
+
+    public void clickViewPlans() {
+        Navigate.clickElement(driver.findElement(viewPlansBy));
     }
 }
--- src/test/java/com/cognizant/tests/TravelInsuranceTest.java
+++ src/test/java/com/cognizant/tests/TravelInsuranceTest.java
@@ -4,7 +4,9 @@ import com.cognizant.Utilities.DriverSetup;
 import com.cognizant.Utilities.Navigate;
 import com.cognizant.homepage.HomePagePO;
 import com.cognizant.travelinsurance.TravelInsurancePO;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.*;
@@ -40,12 +42,42 @@ public class TravelInsuranceTest {
     }
 
 
+//    @Test(alwaysRun = false)
+//    public void testNavigateToPage() {
+//        HomePagePO homepage = new HomePagePO(driver);
+//        homepage.openHomePage();
+//        TravelInsurancePO travelInsurancePage = homepage.gotoTravelInsurance();
+//        String pageTitle = Navigate.getTitle(driver);
+//        Assert.assertEquals(pageTitle, "PolicyBazaar Travel Insurance");
+//    }
+
     @Test
-    public void testNavigateToPage() {
-        HomePagePO homepage = new HomePagePO(driver);
-        homepage.openHomePage();
-        TravelInsurancePO travelInsurancePage = homepage.gotoTravelInsurance();
-        String pageTitle = Navigate.getTitle(driver);
-        Assert.assertEquals(pageTitle, "PolicyBazaar Travel Insurance");
+    public void testSearchCountry() {
+        // get homepage instance
+        HomePagePO homePage = new HomePagePO(driver);
+        // open homepage url
+        homePage.openHomePage();
+        // click on Travel Insurance Button
+        TravelInsurancePO travelInsurancePage = homePage.gotoTravelInsurance();
+        Navigate.wait(driver, 0);
+        // Enter 'France' in country box
+        travelInsurancePage.searchCountry("France"); // read from apache poi excel
+        // Select age '21' for first traveler
+        travelInsurancePage.selectAge("21", travelInsurancePage.ageSelector0By); // read from apache poi excel
+        // click on add another traveler
+        travelInsurancePage.addAnotherTraveler();
+        // Select age '22' for second traveler
+        travelInsurancePage.selectAge("21", travelInsurancePage.ageSelector1By); // read from apache poi excel
+        // click the 'next' button
+        travelInsurancePage.clickNext();
+        // click on dates from calendar pop-up
+        travelInsurancePage.selectCalendarDates();
+        // click on the proceed button
+        travelInsurancePage.clickProceedButton();
+        // enter mobile number
+        travelInsurancePage.enterMobileNumber("9876543210"); // read from apache poi excel
+        // click on 'View Plans'
+        travelInsurancePage.clickViewPlans();
+
     }
 }
