5f71a9d1a96833255ef538f1a1b14ed22f3c7b0c
Swarnima Pandey <swarnima26796@gmail.com>
2021-06-21 14:51:16 +0530

    Wrote a common code for reading excel data from multiple sheets for TravelInsurance as well as CarInsurance module. data is Inputdata.xlsx under resources didn't update data for carinsurance PLEASE REVIEW THE READEXCEL CODE AND REVERT

--- src/main/java/com/cognizant/main/main.java
+++ src/main/java/com/cognizant/main/main.java
@@ -2,14 +2,24 @@ package com.cognizant.main;
 
 import com.cognizant.CarInsurance.CarInsurancePage2;
 import com.cognizant.Utilities.DriverSetup;
+import com.cognizant.apachePOI.ReadExcel;
 import com.cognizant.configuration.Configuration;
 import com.cognizant.homepage.HomePagePO;
 import com.cognizant.travelinsurance.TravelInsurancePO;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.WebDriver;
 
+import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.CancellationException;
 
 public class main {
-    public static void main(String[] args) { }
+    public static void main(String[] args) throws IOException {
+        ReadExcel ex = new ReadExcel("Inputdata.xls", 0);
+        Map<String, Map<String, String>> excelData =ex.getExcelAsMap();
+
+
+
+    }
 }
